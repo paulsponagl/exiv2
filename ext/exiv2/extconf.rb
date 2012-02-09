c5f588731ae8b4f8bed49728493ae1d2252b5603
Paul Sponagl <paul@sponagl.de>
2012-02-09 09:27:15 +0100

    add /usr/local/include to $INCFLAGS to help mkmf in finding headers

--- ext/exiv2/extconf.rb
+++ ext/exiv2/extconf.rb
@@ -1,4 +1,5 @@
 require 'mkmf'
+$INCFLAGS << ' -I/usr/local/include '
 dir_config("exiv2")
-have_library("exiv2")
-create_makefile("exiv2/exiv2")
+have_library("exiv2")  
+create_makefile("exiv2/exiv2") 
